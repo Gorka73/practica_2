62ae57f50ca137f6dfa33b5c67ef0e84235caec7
TheReal73 <gorka.sagri.novell7@gmail.com>
2024-10-28 10:04:50 +0100

    punt 2 acabat.

--- codi.c
+++ codi.c
@@ -7,13 +7,13 @@ float MatDD[N][N];
 float V1[N],V2[N],V3[N],V4[N];
 
 void InitData(){
-    int i,j;
+    int i, j;
     srand(334411);
     for( i = 0; i < N; i++ )
         for( j = 0; j < N; j++ ){
-            Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+            Mat[i][j]= ( ((i * j)%3) ? -1 : 1 ) * (100.0 * ( rand() / (1.0 * RAND_MAX)) );
             if ( (abs(i - j) <= 3) && (i != j))
-                MatDD[i][j] = (((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX));
+                MatDD[i][j] = ( ((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX));
             else if ( i == j )
                 MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX)));
             else MatDD[i][j] = 0.0;
@@ -28,22 +28,38 @@ void InitData(){
 // 1. Per a comprovar que el codi funciona:
 void PrintVect( float vect[N], int from, int numel ){
     if (from < 0 || from >= N || from + numel > N) {
-        printf("Índices fuera de límites.\n");
+        printf("Índexs fora de límits.\n");
         return;
     }
 
     for (int i = from; i < from + numel; i++) {
         printf("%.2f ", vect[i]);
     }
-    printf("\n"); 
+    printf("\n");
 }
 
 // 2. Amb el mateix objectiu, desenvoluparem una funció que mostri per pantalla un cert nombre d’elements (numel) d’una fila (row) d’una matriu a partir d’una posició donada (from).
-void PrintRow( float  mat[N][N], int row, int numel)
+void PrintRow( float  mat[N][N], int row, int numel){
+    if (row < 0 || row >= N) {
+        printf("Índex de fila fora de límits.\n");
+        return;
+    }
+    if (numel < 1 || numel > N) {
+        printf("Nombre d'elements fora de límits.\n");
+        return;
+    }
+
+    for (int aa = 0; aa < numel; aa++) {
+        printf("%.2f ", mat[row][aa]);
+    }
+    printf("\n");
+}
+
 
 int main() {
     InitData();
-    PrintVect(V1, 2, 5); 
+    PrintVect(V1, 2, 5);
+    PrintRow(Mat, 3, 5);
 
     return 0;
 }
